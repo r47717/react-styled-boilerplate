30d2d026263a446a3420b9d4b2032282154620cd
Mikhail Chernorutsky <mikhail.chernorutsky@ontarget-group.com>
2021-04-04 11:50:33 +0300

    initial commit

---
+++ src/components/App/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div`
+  max-width: 960px;
+  margin: 0 auto;
+`
+
+export function App() {
+  return <Container>test</Container>
+}
